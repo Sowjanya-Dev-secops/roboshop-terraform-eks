d7fd91d7e4177c6588a37e872074567e7197ee4b
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-03-04 13:07:01 +0400

    terraform

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -14,22 +14,22 @@ variable "zone_id" {
 variable "domain_name" {
   default = "sgrdevsecops.fun"
 }
-variable "eks_version" {
+# variable "eks_version" {
 
-}
+# }
 
-variable "eks_nodegroup_blue_version" {
+# variable "eks_nodegroup_blue_version" {
 
-}
+# }
 
-variable "eks_nodegroup_green_version" {
+# variable "eks_nodegroup_green_version" {
 
-}
+# }
 
-variable enable_blue {
+# variable enable_blue {
 
-}
+# }
 
-variable enable_green {
+# variable enable_green {
     
-}
+# }
